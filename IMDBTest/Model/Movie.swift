89a5188be87e92c3df67838469c3b12f735252b6
Tono <aff128312@gmail.com>
2021-08-05 15:31:31 -0500

    Add Movie model and Detail VC

--- IMDBTest/Model/Movie.swift
+++ IMDBTest/Model/Movie.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct Movie: Decodable {
+    var id: String?
+    var image: String?
+    var title: String?
+    var year: String?
+    var genres: String?
+    var countries: String?
+    var plot: String?
+    var directors: String?
+}
+
